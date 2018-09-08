127f8a7286d3612dbf7a2ce886620f699b3c9712
boiz <hairypriest@hotmail.com>
2018-09-07 18:24:52 -0700

    beta ajax

--- mine.js
+++ mine.js
@@ -1,12 +1,3 @@
-
-/*Last modified Apr 2018*/
-const getXHR=(url,callback)=>{
-	const xml=new XMLHttpRequest;
-	xml.open("get",url);
-	xml.send();
-	xml.responseType="json";
-	xml.onload=()=>callback(xml.response);
-}
 /*Last modified 2017*/
 let getFormData=data=>{
 	var param="";
@@ -40,6 +31,38 @@ const postForm=obj=>{
 	xml.open("post",obj.url);
 	xml.responseType="json";
 	xml.send(fd);
+	xml.onload=()=>{
+		if(obj.callback) obj.callback(xml.response);
+	}
+}
+
+/*sep 7 2018*/
+const getForm=obj=>{
+	const xml=new XMLHttpRequest;
+	xml.open("get",obj.url);
+	xml.send();
+	xml.responseType="json";
+	xml.onload=()=>{
+		if(obj.callback) obj.callback(xml.response);
+	}
+}
+
+/*beta sep 7 2018*/
+const ajax=obj=>{
+	const xml=new XMLHttpRequest;
+	xml.open(obj.method,obj.url);
+	xml.responseType="json";
+
+	if(obj.method=="get"){
+		xml.send();
+	}
+
+	else if(obj.method=="post"){
+		const fd=new FormData;
+		for(key in obj.data) fd.append(key,obj.data[key]);
+		xml.send(fd);
+	}
+
 	xml.onload=()=>{
 		if(obj.callback) obj.callback(xml.response);
 	}
